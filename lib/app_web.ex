9c1f999d3959d4d7ecef92a86b21f066771f0e45
SimonLab <simon.labondance@gmail.com>
2022-10-04 11:14:40 +0100

    Use Phoenix.Component module
    Latest Phoenix.LiveView version requires Phoenix.Component module
    to be imported for some view helper function to be found

--- lib/app_web.ex
+++ lib/app_web.ex
@@ -33,6 +33,8 @@ defmodule AppWeb do
         namespace: AppWeb
 
       # Import convenience functions from controllers
+      import Phoenix.Component
+
       import Phoenix.Controller,
         only: [get_flash: 1, get_flash: 2, view_module: 1, view_template: 1]
 
